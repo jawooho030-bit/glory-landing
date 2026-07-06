a1e1461915c2ea15b92d3d51fcfdcbf7b3ca9538
jwzy <jawooho030@gmail.com>
2026-07-06 15:11:59 +0900

    Update line.js

--- js/line.js
+++ js/line.js
@@ -3,7 +3,7 @@
 // ONLY SOURCE OF LINE_URL
 // =========================
 
-const LINE_URL = "https://line.me/ti/p/484lolur";
+const LINE_URL = "https://line.me/ti/p/@484lolur";
 
 function openLine(source = "unknown") {
   console.log("LINE click:", source);
