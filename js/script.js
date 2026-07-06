f86f6183565a6946f255c6390afec6b0ef17d01e
jwzy <jawooho030@gmail.com>
2026-07-06 11:53:35 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+window.addEventListener("scroll", () => {
+  const btn = document.getElementById("floatingLine");
 
+  if (window.scrollY > 300) {
+    btn.style.opacity = "1";
+  } else {
+    btn.style.opacity = "1";
+  }
+});
