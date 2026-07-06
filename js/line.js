8bc6a24dad24e00071fd7d030a1734c8ef1ff4a4
jwzy <jawooho030@gmail.com>
2026-07-06 16:16:29 +0900

    Update line.js

--- js/line.js
+++ js/line.js
@@ -3,21 +3,22 @@
 // Meta Pixel + LINE
 // =========================
 
+// 建議之後換成 https://lin.ee/XXXXXX
 const LINE_URL = "https://line.me/ti/p/@484lolur";
 
 function openLine(source = "unknown") {
 
   console.log("LINE click:", source);
 
-  // Meta Pixel Contact Event
+  // Meta Pixel：記錄使用者點擊 LINE
   if (typeof fbq !== "undefined") {
     fbq("track", "Contact", {
       content_name: source
     });
   }
 
-  // 等待事件送出再開啟 LINE
-  setTimeout(function () {
+  // 等待 Pixel 事件送出
+  setTimeout(() => {
     window.open(LINE_URL, "_blank");
   }, 200);
 
